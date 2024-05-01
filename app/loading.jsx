18a73d3cc3524824b00dad6b99dd0fb3058a792f
Projects <artur_kulembetov@outlook.com>
2024-05-01 19:36:17 +0700

    feat: add not found and loading page

---
+++ app/loading.jsx
@@ -0,0 +1,19 @@
+"use client";
+import { ClipLoader } from "react-spinners";
+const LoadingPage = ({ loading }) => {
+  const override = {
+    display: "block",
+    margin: "100px auto",
+  };
+  return (
+    <ClipLoader
+      color="#3b82f6"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+      aria-label="Loading spinner"
+    />
+  );
+};
+
+export default LoadingPage;
