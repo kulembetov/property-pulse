4ac8c2ccaf2c2f5100bde2357d81be78782dc881
Projects <artur_kulembetov@outlook.com>
2024-05-04 19:45:08 +0700

    feat: add next auth, sessions and google provider

---
+++ middleware.js
@@ -0,0 +1,10 @@
+export { default } from 'next-auth/middleware';
+
+export const config = {
+  matcher: [
+    '/properties/add',
+    '/properties/profile',
+    '/properties/saved',
+    '/messages',
+  ],
+};
