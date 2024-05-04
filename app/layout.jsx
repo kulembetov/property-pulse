edd4041383569a3866f1182cc9be995c060b1cca
Projects <artur_kulembetov@outlook.com>
2024-05-04 19:45:08 +0700

    feat: add next auth, sessions and google provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "@/assets/styles/globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata = {
   title: "Property Pulse | Find The Perfect Rental",
@@ -10,13 +11,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <NavBar />
-        <div>{children}</div>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <NavBar />
+          <div>{children}</div>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
