b759b482922e46d3177eddba981dfd4fa1c8fa38
Projects <artur_kulembetov@outlook.com>
2024-05-14 22:04:27 +0700

    feat: implement geocoding and mapbox, bookmarks, saved properties list, share buttons

---
+++ components/PropertyMap.jsx
@@ -0,0 +1,85 @@
+"use client";
+import pin from "@/assets/images/pin.svg";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map, { Marker } from "react-map-gl";
+import { setDefauls, fromAddress, setDefaults } from "react-geocode";
+import React, { useEffect, useState } from "react";
+import Spinner from "@/components/Spinner";
+import Image from "next/image";
+
+const PropertyMap = ({ property }) => {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [viewPort, setViewPort] = useState({
+    latitude: 0,
+    longitude: 0,
+    zoom: 12,
+    width: "100%",
+    height: "500px",
+  });
+  const [loading, setLoading] = useState(true);
+  const [geocodeError, setGeocodeError] = useState(false);
+
+  setDefaults({
+    key: process.env.NEXT_PUBLIC_GOOGLE_GEOCODING_API_KEY,
+    language: "en",
+    region: "us",
+  });
+
+  useEffect(() => {
+    const fetchCoordinates = async () => {
+      try {
+        const res = await fromAddress(
+          `${property.location.street} ${property.location.city} ${property.location.state} ${property.location.zipcode}`
+        );
+
+        if (res.results.length === 0) {
+          setGeocodeError(true);
+          setLoading(false);
+          return;
+        }
+
+        const { lat: latitude, lng: longitude } =
+          res.results[0].geometry.location;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+        setViewPort({ ...viewPort, latitude: latitude, longitude: longitude });
+        setLoading(false);
+      } catch (e) {
+        console.error(e);
+        setGeocodeError(true);
+        setLoading(false);
+      }
+    };
+    fetchCoordinates();
+  }, []);
+
+  if (loading) return <Spinner loading={loading} />;
+
+  if (geocodeError) {
+    return <div className="text-xl">No location data found</div>;
+  }
+
+  return (
+    !loading && (
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        mapLib={import("mapbox-gl")}
+        initialViewState={{
+          longitude: longitude,
+          latitude: latitude,
+          zoom: 15,
+        }}
+        style={{ width: "100%", height: 500 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <Marker latitude={latitude} longitude={longitude} anchor="bottom">
+          <Image src={pin} alt="location" width={40} height={40} />
+        </Marker>
+      </Map>
+    )
+  );
+};
+
+export default PropertyMap;
