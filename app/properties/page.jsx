82a2c5faf952583973e3366d6c03aac6de7247aa
Projects <artur_kulembetov@outlook.com>
2024-05-15 14:30:07 +0700

    feat: implement property search

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,6 +1,9 @@
-import React from "react";
 import PropertyCard from "@/components/PropertyCard";
 import { fetchProperties } from "@/utils/requests";
+import PropertySearchForm from "@/components/PropertySearchForm";
+import Spinner from "@/components/Spinner";
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
@@ -8,19 +11,27 @@ const PropertiesPage = async () => {
   properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto">
-        {properties.length === 0 ? (
-          <p>No properties found</p>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl max-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto">
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+      );
+    </>
   );
 };
 
